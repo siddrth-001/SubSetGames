30b69e3db5061e3d5785b8488a657aee348dacfc
siddrth-001 <siddharth.1822co1057@kiet.edu>
2024-09-11 09:59:48 +0530

    AIR-Force

---
+++ Assets/VoiceGame/Scripts/AudioDetect.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioDetect : MonoBehaviour
+{
+    public int sampleWindow = 64;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // public float GetLoudness(int clipPosition, AudioClip clip)
+    // {
+    //    int startPosition
+    // }
+
+}
